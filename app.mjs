12a28941f4a433db4783f7f57d3e05ee8dc71ce3
Pedro León <pedro@wintercr.com>
2024-09-24 23:16:02 -0500

    Added basic sample of simulating an online drawing

--- app.mjs
+++ app.mjs
@@ -1,17 +1,41 @@
 import express from "express"
 import cors from "cors"
+import { Server } from "socket.io"
+import http from "http"
+import fs from "fs"
+const FILE_PATH = './document.txt';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server);
+app.use(express.static('public'));
 
 
-app.get("/", (req, res) => {
-  return res.status(200).send("Hello World!")
-})
+
+io.on('connection', (socket) => {
+  // Load initial file content on connection
+  fs.readFile(FILE_PATH, 'utf8', (err, data) => {
+      if (!err) {
+          socket.emit('receiveEdit', data);
+      }
+  });
+
+  socket.on('edit', (data) => {
+      socket.broadcast.emit('receiveEdit', data);
+
+      // Write data to file
+      fs.writeFile(FILE_PATH, data, (err) => {
+          if (err) {
+              console.error('Error writing to file:', err);
+          }
+      });
+  });
+});
 
 
 const port = 8080;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
