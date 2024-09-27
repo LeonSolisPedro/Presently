0ada7d1a9375e20442271b8e7d43f3cad264e033
Pedro León <pedro@wintercr.com>
2024-09-27 14:20:12 -0500

    Adding realtime collaboration trught rooms, there's a problem with the bursh

--- app.mjs
+++ app.mjs
@@ -3,6 +3,8 @@ import cors from "cors"
 import { Server } from "socket.io"
 import http from "http"
 import fs from "fs"
+import fss from "fs/promises"
+import { Readable } from "stream"
 const FILE_PATH = './document.txt';
 
 const app = express();
@@ -16,28 +18,70 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
 
-    socket.on('addCircle', (data) => {
-        socket.broadcast.emit('receiveCircle', data);
-    });
-    socket.on('addSquare', (data) => {
-        socket.broadcast.emit('receiveSquare', data);
-    });
-    socket.on('addLine', (data) => {
-        socket.broadcast.emit('receiveLine', data);
-    });
-    socket.on('addLineArrow', (data) => {
-        socket.broadcast.emit('receiveLineArrow', data);
-    });
-    socket.on('objectModified', (data) => {
-        socket.broadcast.emit('recieveObjectModified', data)
-    })
-    socket.on('drawing', (string) => {
-        socket.broadcast.emit('drawing', string);
-    });
-    socket.on('addText', (string) => {
-        socket.broadcast.emit('receiveText', string);
-    });
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+  });
+
+  socket.on('leaveRoom', (room) => {
+    socket.leave(room);
+  });
+
+
+  socket.on('addCircle', (data, room) => {
+    socket.to(room).emit('receiveCircle', data);
+  });
+
+  socket.on('addSquare', (data, room) => {
+    socket.to(room).emit('receiveSquare', data);
+  });
+
+  socket.on('addLine', (data, room) => {
+    socket.to(room).emit('receiveLine', data);
+  });
+
+  socket.on('addLineArrow', (data, room) => {
+    socket.to(room).emit('receiveLineArrow', data);
+  });
+
+  socket.on('objectModified', (data, room) => {
+    socket.to(room).emit('receiveObjectModified', data);
+  });
+
+  socket.on('drawing', (string, room) => {
+    socket.to(room).emit('drawing', string);
+  });
+
+  socket.on('addText', (string, room) => {
+    socket.to(room).emit('receiveText', string);
+  });
+
+  socket.on('deleteObject', (string, room) => {
+    socket.to(room).emit('deleteObject', string);
+  });
+
+  socket.on('saveDataDisk', (string, room) => {
+    const ostream = fs.createWriteStream(`./slides/${room}/slide1.json`, { encoding: 'utf8' });
+    const readable = Readable.from(string);
+    readable.pipe(ostream);
+  });
 });
+
+
+app.get("/api/getSlide/:id", async (req, res) => {
+  const { id } = req.params;
+  const { page } = req.query;
+  try {
+    await fss.access(`slides/${id}`)
+    const files = await fss.readdir(`slides/${id}`)
+    const buffer = await fss.readFile(`slides/${id}/slide${page ?? 1}.json`);
+    const jsonData = JSON.parse(buffer);
+    const response = { numberOfSlides: files.length, current: page ?? 1, content: jsonData }
+    return res.status(200).json(response)
+  } catch (error) {
+    return res.status(404).send()
+  }
+})
+
 
 
 const port = 8080;
